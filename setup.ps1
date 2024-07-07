fc40feaf3681691e03a64c5e0511876662e6603d
saharaku91 <77642408+saharaku91@users.noreply.github.com>
2024-07-07 19:45:23 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,14 @@
+# Disable the firewall for Domain, Public, and Private profiles
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+
+# Download and install Chrome Remote Desktop Host
+$CRDPath = "$env:TEMP\chromeremotedesktophost.msi"
+Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $CRDPath
+Start-Process $CRDPath -Wait
+Remove-Item $CRDPath
+
+# Download and install Google Chrome
+$ChromePath = "$env:TEMP\chrome_installer.exe"
+Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $ChromePath
+Start-Process -FilePath $ChromePath -ArgumentList '/silent', '/install' -Verb RunAs -Wait
+Remove-Item $ChromePath
